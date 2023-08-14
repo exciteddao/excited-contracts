40995e59bb5a4e4beadadf2625383d523371cd6c
Shahar Yakir <shahar.yakir@gmail.com>
2023-08-14 13:51:17 +0100

    empty contract with failing test, folder setup

---
+++ contracts/vesting-v1/VestingV1.sol
@@ -0,0 +1,1 @@
+contract VestingV1 {}
---
+++ test/vesting-v1.test.ts
@@ -0,0 +1,10 @@
+import { block, bn18 } from "@defi.org/web3-candies";
+import { expect } from "chai";
+import BN from "bignumber.js";
+import { expectRevert } from "@defi.org/web3-candies/dist/hardhat";
+
+describe("VestingV1", () => {
+  it("fails", async () => {
+    throw new Error("Not implemented");
+  });
+});
