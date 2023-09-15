0cb19abd22981a020d1a0f8d19677b1b60cdbc64
Sukh Singh <sukh@orbs.com>
2023-09-15 15:16:34 +0100

    fixed types and render correct spec of abi based on types

--- contracts-ui/src/App.tsx
+++ contracts-ui/src/App.tsx
@@ -2,16 +2,19 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { VestingContract } from "./VestingContract";
 import Web3 from "web3";
 import { AppConfig } from "./config";
+import VestingAbi from "./generated/contracts/vesting-v1/VestingV1.json";
+import { VestingV1 } from "./generated/contracts/vesting-v1/VestingV1";
 
 const queryClient = new QueryClient();
 const web3 = new Web3(AppConfig.Polygon.Mainnet.RpcUrl);
+const vestingContract = new web3.eth.Contract(VestingAbi.abi, AppConfig.Polygon.Mainnet.VestingV1ContractAddress) as unknown as VestingV1;
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <h1>Excited Contracts</h1>
       <div>
-        <VestingContract web3={web3} />
+        <VestingContract contract={vestingContract} />
       </div>
     </QueryClientProvider>
   );
--- contracts-ui/src/VestingContract.tsx
+++ contracts-ui/src/VestingContract.tsx
@@ -1,17 +1,78 @@
-import { useQuery } from "react-query";
-import VestingAbi from "./contracts/VestingV1/VestingV1.json";
-import { AppConfig } from "./config";
-import { Web3 } from "web3";
+import VestingAbi from "./generated/contracts/vesting-v1/VestingV1.json";
+import { VestingV1 } from "./generated/contracts/vesting-v1/VestingV1";
+import { useState } from "react";
+import { NonPayableTransactionObject } from "./generated/types";
 
-export function VestingContract({ web3 }: { web3: Web3 }) {
-  const { isLoading, error, data } = useQuery("vestingContract", async () => {
-    const vesting = new web3.eth.Contract(VestingAbi.abi, AppConfig.Polygon.Mainnet.VestingV1ContractAddress);
-    return vesting.methods.MAX_VESTING_DURATION_SECONDS().call();
-  });
+type AbiItemProps = {
+  contract: VestingV1;
+  abi: (typeof VestingAbi.abi)[number];
+};
 
-  if (isLoading) return <h3>Loading...</h3>;
+function AbiItem({ contract, abi }: AbiItemProps) {
+  const [result, setResult] = useState<string>("");
 
-  if (error) return <h3>Error: {JSON.stringify(error)}</h3>;
+  const inputsString = abi.inputs
+    .filter(Boolean)
+    .map((input: { name?: string; type: string }, inputIndex) => `${input.name || `arg${inputIndex}`}: ${input.type}`)
+    .join(", ");
 
-  return <h3>{`${data}`}</h3>;
+  const outputsString =
+    abi.outputs && abi.outputs.length > 0
+      ? [
+          ":",
+          abi.outputs
+            .filter(Boolean)
+            .map((output: { name?: string; type: string }) => output.type)
+            .join(", "),
+        ].join(" ")
+      : "";
+
+  return (
+    <div style={{ borderBottom: "2px solid #f00", marginBottom: "40px" }}>
+      <h3>{[abi.type, abi.name, "(", inputsString, ")", outputsString].filter(Boolean).join(" ")}</h3>
+      {abi.name && abi.type === "function" && abi.outputs && abi.outputs.length > 0 && (
+        <>
+          <div>{result}</div>
+          <button
+            onClick={() => {
+              const methodName: keyof typeof contract.methods = abi.name as keyof typeof contract.methods;
+              const args = abi.inputs.map((input) => input.name || "");
+              console.log("args", args);
+
+              // eslint-disable-next-line prefer-spread
+              const functionToCall = (contract.methods[methodName] as (...args: unknown[]) => NonPayableTransactionObject<unknown>).apply(null, args);
+              console.log(functionToCall);
+              functionToCall.call().then((res: unknown) => {
+                setResult(`Result: ${res}`);
+              });
+            }}
+          >
+            Run
+          </button>
+        </>
+      )}
+    </div>
+  );
+}
+
+type VestingContractProps = {
+  contract: VestingV1;
+};
+
+export function VestingContract({ contract }: VestingContractProps) {
+  const Abi = VestingAbi.abi
+    .sort((a, b) => {
+      if (a.type === "function" && b.type !== "function") {
+        return -1;
+      }
+
+      if (a.type !== "function" && b.type === "function") {
+        return 1;
+      }
+
+      return 0;
+    })
+    .map((abi, index) => <AbiItem key={index} contract={contract} abi={abi} />);
+
+  return <div>{Abi}</div>;
 }
